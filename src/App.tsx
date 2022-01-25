839a519c24aa20699542c6a393e9c756d1794899
André Prado <andre@pixelcart.com.br>
2022-01-25 20:29:55 -0300

    *added total investment and list of expenses

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Empty } from 'antd';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import ListItem from './components/ListItem';
 import { ITransactions } from './interfaces/ITransactions';
+import { roundUp } from './utils/calc';
 
 const App: React.FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [content, setContent] = useState<ITransactions[]>([]);
+
+    const [investment, setInvestment] = useState<number>(0);
 
     useEffect(() => {
         const transactions_array: ITransactions[] = [];
@@ -16,15 +20,20 @@ const App: React.FC = () => {
             })
             .then((data) => {
                 data.transactions.forEach((transaction: any) => {
+                    const invest = roundUp(
+                        roundUp(-transaction.amount_cents / 100, 0) -
+                            -transaction.amount_cents / 100,
+                        3
+                    );
+                    setInvestment((c) => c + invest);
                     const item = {
                         id: transaction.id,
-                        amount_cents: transaction.amount_cents,
+                        amount_cents: -transaction.amount_cents,
                         date: transaction.date,
                         description: transaction.description,
                     };
                     transactions_array.push(item);
                 });
-                console.log(transactions_array);
                 setContent(transactions_array);
                 setIsLoading(false);
             })
@@ -33,13 +42,16 @@ const App: React.FC = () => {
 
     return (
         <>
-            {content && content.length > 0 ? (
-                content.map((transaction: ITransactions, key: number) => {
-                    return <ListItem key={key} transaction={transaction} />;
-                })
-            ) : (
-                <p>Nothing to show here..</p>
-            )}
+            {roundUp(investment, 2)}
+            <ul>
+                {content && content.length > 0 ? (
+                    content.map((transaction: ITransactions, key: number) => {
+                        return <ListItem key={key} transaction={transaction} />;
+                    })
+                ) : (
+                    <Empty />
+                )}
+            </ul>
         </>
     );
 };
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { ITransactions } from '../interfaces/ITransactions';
-
+import { roundUp } from '../utils/calc';
 interface Props {
     transaction: ITransactions;
 }
 
 const ListItem: React.FC<Props> = ({ transaction }: Props) => {
     return (
-        <>
-            <p>{transaction.id}</p>
-        </>
+        <li>
+            {transaction.description} - R$ {transaction.amount_cents / 100} -
+            Round Up: {roundUp(transaction.amount_cents / 100, 0)} - Investment:{' '}
+            {roundUp(
+                roundUp(transaction.amount_cents / 100, 0) -
+                    transaction.amount_cents / 100,
+                3
+            )}
+        </li>
     );
 };
 
